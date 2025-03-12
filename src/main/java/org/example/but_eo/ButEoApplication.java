32b69c0d9d942ac591f98a86e2b3842b741ef111
SangHyun Oh <132821751+ohsang14@users.noreply.github.com>
2025-03-12 23:08:29 +0900

    초기설정

---
+++ src/main/java/org/example/but_eo/ButEoApplication.java
@@ -0,0 +1,13 @@
+package org.example.but_eo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButEoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ButEoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/but_eo/ButEoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.but_eo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButEoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
