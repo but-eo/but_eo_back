bd24a4cb24b589d9a45eada9f80ba9b340b07ffb
zaldo232 <zaldo232@naver.com>
2025-03-28 00:01:33 +0900

    로그인,회원가입_jwt
    인증완료

--- src/main/java/org/example/but_eo/util/JwtUtil.java
+++ src/main/java/org/example/but_eo/util/JwtUtil.java
@@ -5,15 +5,14 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    // 512bit 이상 키 자동 생성
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
-
+    private final SecretKey key = Keys.hmacShaKeyFor(JwtConstants.SECRET.getBytes());
     private final long EXPIRATION_TIME = 1000 * 60 * 60 * 24; // 24시간
 
     public String generateToken(String userId) {
