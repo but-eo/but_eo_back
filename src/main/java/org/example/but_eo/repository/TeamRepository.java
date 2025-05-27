3a1b28d71f4509c3cfb7b33d16281e783b7f6620
SangHyun Oh <132821751+ohsang14@users.noreply.github.com>
2025-05-27 14:17:42 +0900

    Update TeamRepository.java

--- src/main/java/org/example/but_eo/repository/TeamRepository.java
+++ src/main/java/org/example/but_eo/repository/TeamRepository.java
@@ -7,6 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TeamRepository extends JpaRepository<Team, String> {
-    List<TeamMember> findByUser_UserHashIdAndType(String userHashId, TeamMember.Type type);
-
 }
