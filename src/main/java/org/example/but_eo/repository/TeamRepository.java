ad0286861dce5baa1d9c89ecadcccd334405c089
zaldo232 <zaldo232@naver.com>
2025-05-27 12:44:19 +0900

    자기가 리더인 팀 조회

--- src/main/java/org/example/but_eo/repository/TeamRepository.java
+++ src/main/java/org/example/but_eo/repository/TeamRepository.java
@@ -1,7 +1,12 @@
 package org.example.but_eo.repository;
 
 import org.example.but_eo.entity.Team;
+import org.example.but_eo.entity.TeamMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TeamRepository extends JpaRepository<Team, String> {
+    List<TeamMember> findByUser_UserHashIdAndType(String userHashId, TeamMember.Type type);
+
 }
