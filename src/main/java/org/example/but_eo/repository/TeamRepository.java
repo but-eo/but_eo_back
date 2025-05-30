a16aec981f568df1a126c93b8919bbc50e8f42a2
zaldo232 <zaldo232@naver.com>
2025-05-30 17:26:52 +0900

    체크좀

--- src/main/java/org/example/but_eo/repository/TeamRepository.java
+++ src/main/java/org/example/but_eo/repository/TeamRepository.java
@@ -3,8 +3,20 @@ package org.example.but_eo.repository;
 import org.example.but_eo.entity.Team;
 import org.example.but_eo.entity.TeamMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, String> {
+
+    @Query("""
+    SELECT t FROM Team t
+    LEFT JOIN FETCH t.teamMemberList tm
+    LEFT JOIN FETCH tm.user
+    WHERE t.teamId = :teamId
+""")
+    Optional<Team> findWithMembersByTeamId(@Param("teamId") String teamId);
+
 }
