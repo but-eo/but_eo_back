a571514e779c1625618d25fb0012ad3e4507bc04
zaldo232 <zaldo232@naver.com>
2025-06-05 17:35:53 +0900

    createdAt으로

--- src/main/java/org/example/but_eo/dto/CommentResponse.java
+++ src/main/java/org/example/but_eo/dto/CommentResponse.java
@@ -16,7 +16,7 @@ public class CommentResponse {
     private String userHashId;
     private String userName;
     private String content;
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
     private int likeCount;
 }
 
